bacedc0583b51c7316f7cdc9bc15465cccedf066
root <6274@holbertonstudents.com>
2023-07-19 18:49:39 -0700

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,7 @@ class BaseModel:
         """
 
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.name__
+        obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
