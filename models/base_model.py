fdc4e7588d66d8930cb3d21aac6d505ce182c83c
root <6274@holbertonstudents.com>
2023-07-19 19:00:17 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                if key =! '__class__':
+                if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
