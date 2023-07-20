c7add0eec3d5bc1ba0c0f1c795b7607ea8c31482
root <6274@holbertonstudents.com>
2023-07-19 18:58:16 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,23 @@ from datetime import datetime
 class BaseModel:
     """Base class for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
         Sets the instance attributes if, created_at,
         and updated_at.
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key =! '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
