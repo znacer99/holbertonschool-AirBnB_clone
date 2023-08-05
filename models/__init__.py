badbe106ed8742be20babb2b35fdfa708bea20dc
znacer99 <124281258+znacer99@users.noreply.github.com>
2023-08-05 16:07:35 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-
-# create a unique FileStorage instance for your application.
-storage = FileStorage()
-
-# Call reload() method on this variable to load objects from
-# the json file.
-storage.reload()
