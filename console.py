bbeeebc2baa30847799140197a3c07323c459c56
root <znacer99>
2023-08-05 09:24:47 -0700

    fix

--- console.py
+++ console.py
@@ -6,16 +6,22 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Exit the program"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
+        """Exit the program when EOF (ctrl+D) is encountered"""
         return True
 
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
+
+    def Default(self, line):
+        """Default behavior for an unknown command"""
+        if line == '':
+            return
+        print(f"*** Unknown command: {line}")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
