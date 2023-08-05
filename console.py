e3631357bb31ea50c70bd1734254fb6097d943b1
root <znacer99>
2023-08-05 09:13:26 -0700

    first

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import cmd
+import models
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
